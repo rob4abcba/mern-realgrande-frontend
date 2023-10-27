450987550ae7d756819899028389f8e4e2060ec9
rob <rob_abcba@yahoo.com>
2023-10-26 17:06:08 -0700

    Move house description to left column because I plan to add PropertyAddress to EnquiryForm in the RightHand column.

--- src/components/SearchedHouse.js
+++ src/components/SearchedHouse.js
@@ -3,44 +3,49 @@ import House from "./House";
 import Enquiry from "./Enquiry";
 
 const SearchedHouse = (props) => {
-     
-    //hooks have to be at the top 
-     let paramsObj = useParams();
-     console.log(paramsObj); 
-     console.log(props); 
- 
-     //find will return the first element in the iterable object that meets the parameters 
-     let findHouseObj = props.houses.find((house) => house._id == paramsObj.id);
-     console.log(findHouseObj);
- 
- 
-     if (!props.houses) {
-         return <p>Loading...</p>;
-     }
- 
-     return (<>
-     <h1>Searched House</h1>
+  //hooks have to be at the top
+  let paramsObj = useParams();
+  console.log(paramsObj);
+  console.log(props);
 
-    <div className="row">
-        <div className="col-sm-6">
-            <b>{findHouseObj.address}</b>  
-            </div>
-        <div className="col-sm-6">
-           <b>Price: USD {findHouseObj.price}</b>
-        </div>
-    </div> 
+  //find will return the first element in the iterable object that meets the parameters
+  let findHouseObj = props.houses.find((house) => house._id == paramsObj.id);
+  console.log(findHouseObj);
 
-    <div className="row">
+  if (!props.houses) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <>
+      <h1>Searched House</h1>
+
+      <div className="row">
         <div className="col-sm-6">
-           <img className="img-fluid" src={'/imgs/'+ findHouseObj.photo} alt="house"/>
-       </div>
-        <div className="col-sm-6">
-           <p>{findHouseObj.description}</p>  
-          {  (sessionStorage.getItem('custname')) && <Enquiry /> }
+          <b>{findHouseObj.address}</b>
         </div>
-    </div>
-         
-     </>);
-}
- 
+        <div className="col-sm-6">
+          <b>Price: ${findHouseObj.price}</b>
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col-sm-6">
+          <img
+            className="img-fluid"
+            src={"/imgs/" + findHouseObj.photo}
+            alt="house"
+          />
+          <p>{findHouseObj.description}</p>
+          {/* <p>{findHouseObj.price}</p> */}
+        </div>
+        <div className="col-sm-6">
+          {/* <p>{findHouseObj.description}</p> */}
+          {sessionStorage.getItem("custname") && <Enquiry />}
+        </div>
+      </div>
+    </>
+  );
+};
+
 export default SearchedHouse;
