481a98df3eb9c126d85683cc64915e919b018e87
rob <rob_abcba@yahoo.com>
2023-10-26 17:00:27 -0700

    Update footer with logo and copyright message.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,21 @@
+import { Link, useNavigate } from "react-router-dom";
 const Footer = () => {
-    return (
-        <div className="row bg-warning"> 
-            <h1>Footer</h1>
-        </div>
-     );
-}
- 
+  return (
+    <div className="row bg-warning d-flex align-items-center">
+      <div className="col-sm-3">
+        <Link to="/">
+          {" "}
+          <img className="logo" src="/imgs/logo.png" alt="logo here" />{" "}
+        </Link>
+      </div>
+      <div className="col-sm-9">
+        <p className="m-0 tagline">
+          {" "}
+          Copyright © 2013-2023 RealGrande Inc. All rights reserved. (
+          v.0.7.1.122 ){" "}
+        </p>
+      </div>
+    </div>
+  );
+};
 export default Footer;
