1ff185a8811ef8da68a0639181ad359cf12c9936
rob <rob_abcba@yahoo.com>
2023-10-26 17:03:24 -0700

    Switch to TWO column format.  With image in left column, all else in right column.

--- src/components/SearchResultsCard.js
+++ src/components/SearchResultsCard.js
@@ -1,4 +1,5 @@
 import { useNavigate } from "react-router-dom";
+import Enquiry from "./Enquiry";
 
 const SearchResultsCard = (props) => {
   const navigate = useNavigate();
@@ -10,7 +11,26 @@ const SearchResultsCard = (props) => {
 
   return (
     <>
-      <div
+      <div className="row" key={props.house._id} onClick={clickHandler}>
+        <div className="col-sm-6">
+          <img
+            src={"/imgs/" + props.house.photo}
+            className="card-img-top img-fluid"
+            alt="house"
+          />
+        </div>
+        <div className="col-sm-6">
+          <h5 class="card-title">Address: {props.house.address}</h5>
+          <p class="card-text">Price: ${props.house.price}</p>
+          <p>{props.house.description}</p>
+          <a href="#" class="btn btn-primary">
+            Enquire for More Information
+          </a>
+          {/* {sessionStorage.getItem("custname") && <Enquiry />} */}
+        </div>
+      </div>
+
+      {/* <div
         class="card"
         // style="width: 18rem;"
         key={props.house._id}
@@ -28,7 +48,8 @@ const SearchResultsCard = (props) => {
             Enquire for More Information
           </a>
         </div>
-      </div>
+      </div> */}
+
       {/* <tr key={props.house._id} onClick={clickHandler}>
         <td scope="card">{props.house.address}</td>
         <td>{props.house.price}</td>
